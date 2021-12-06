c21310bd044fed9a1308c56fdf8cb5c59dd27a16
Alonso <alonso.rapado@hotmail.com>
2021-12-06 14:17:54 +0100

    first working approach

--- final_project.ino
+++ final_project.ino
@@ -4,20 +4,57 @@
 #include "communications.hpp"
 #include <ESP8266WiFi.h>
 
-void setupWindow(){
-  pinMode(WINDOWPIN, OUTPUT);
-  digitalWrite(WINDOWPIN, LOW); 
+String Asciitohex( String data ) {
+
+  // Length (with one extra character for the null terminator)
+  int str_len = data.length() + 1;
+
+  // Prepare the character array (the buffer)
+  char hex[str_len];
+
+  // Copy it over
+  data.toCharArray(hex, str_len);
+  // 'clear' buffer
+  char buffer[sizeof(hex) * 2 + 1];
+
+  buffer[0] = '\0';
+
+  // loop through character array
+  for (uint8_t cnt = 0; cnt < strlen(hex); cnt++)
+  {
+    // conver each character to its hex representation
+    itoa(hex[cnt], &buffer[cnt * 2], 16);
+
+  }
+  // return result
+  return String(buffer);
 }
-String getWindowStatus(){
+
+String hexToAscii( String hex )
+{
+  uint16_t len = hex.length();
+  String ascii = "";
+
+  for ( uint16_t i = 0; i < len; i += 2 )
+    ascii += (char)strtol( hex.substring( i, i + 2 ).c_str(), NULL, 16 );
+
+  return ascii;
+}
+
+void setupWindow() {
+  pinMode(WINDOWPIN, OUTPUT);
+  digitalWrite(WINDOWPIN, LOW);
+}
+String getWindowStatus() {
   return windowStatus;
 }
-void setWindowStatus(){
-  if(windowStatus == "0"){
-    digitalWrite(WINDOWPIN, HIGH); 
+void setWindowStatus() {
+  if (windowStatus == "0") {
+    digitalWrite(WINDOWPIN, HIGH);
     windowStatus = "1";
-  }else{
+  } else {
     digitalWrite(WINDOWPIN, LOW);
-     windowStatus = "0"; 
+    windowStatus = "0";
   }
 }
 
@@ -28,7 +65,7 @@ float v40000 = 3.93;
 SoftwareSerial mySerial(TX, RX);
 rn2xx3 myLora(mySerial);
 String deviceId = "";
-
+String str = "";
 void setup() {
   Serial.begin(57600);
   setupWindow();
@@ -36,7 +73,7 @@ void setup() {
   co2Sensor.begin(v400, v40000);
   //co2Sensor.calibrate();  co2Sensor.calibrate();
   mySerial.begin(9600);
-  
+
   //reset rn2483
   pinMode(RST, OUTPUT);
   digitalWrite(RST, LOW);
@@ -48,8 +85,8 @@ void setup() {
 
   //initialise the rn2483 module
   myLora.autobaud();
+  setupMySerial();
 
-  //print out the HWEUI so that we can register it via ttnctl
   deviceId = String( myLora.hweui() );
 }
 
@@ -66,8 +103,73 @@ void loop() {
   Serial.println(temperature);
   Serial.println(CO2Value);
   Serial.println(windowStatus);
-  String finalData = deviceId  + "," + humidity + "," + temperature + "," + CO2Value + "," + windowStatus;
-  Serial.println(finalData);
+  String finalData = Asciitohex(deviceId  + "," + humidity + "," + temperature + "," + CO2Value + "," + windowStatus);
+  
+  mySerial.println("radio tx " + finalData);
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
 
-  delay(300);
+  delay(10000);
+}
+
+void setupMySerial() {
+  Serial.println("Initing LoRa");
+  
+  mySerial.println("mac pause");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set mod lora");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set freq 869100000");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set pwr 14");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set sf sf7");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set afcbw 41.7");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set rxbw 125");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set prlen 8");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set crc on");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set iqi off");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set cr 4/5");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  /*mySerial.println("radio set wdt 60000"); //disable for continuous reception
+    str = mySerial.readStringUntil('\n');
+    Serial.println(str);*/
+
+  mySerial.println("radio set sync 12");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
+
+  mySerial.println("radio set bw 125");
+  str = mySerial.readStringUntil('\n');
+  Serial.println(str);
 }
